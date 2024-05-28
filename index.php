23b589f652ea1f807066b066e6156525d106e0d0
Furkan Yanteri <furkanyanteri@gmail.com>
2024-05-28 15:53:10 +0300

    init: php playground

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<bodv>
+<main> 
+    <form action="includes/formhandler.php" method="post">
+        <label for="firstname">Firstname?</label>
+        <input id="firstname" type="text" name="firstname" placeholder="Firstname..." >
+
+        <label for= "lastname" >Lastname?</label>
+        <input id="lastname" type="text" name= "lastname" placeholder="Lastname..." >
+
+        <label for="favouritepet" >Favourite Pet?</label>
+        <select id="favouritepet" name="favouritepet"> 
+            <option value = "none" >None</option> 
+            <option value = "dog" >Dog</option> 
+            <option value = "cat" >Cat</option> 
+            <option value = "bird">Bird</option>
+        </select>
+        <button type="submit">Submit</button> 
+</form>
+</main>
+    
+</body>
+</html>
